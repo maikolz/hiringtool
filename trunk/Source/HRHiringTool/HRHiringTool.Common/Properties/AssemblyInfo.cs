13062af14cee8a5783dfb94ddfbfa1bec61718e0
maikolz@gmail.com <maikolz@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-06-14 16:29:16 +0000

    Adding common

--- trunk/Source/HRHiringTool/HRHiringTool.Common/Properties/AssemblyInfo.cs
+++ trunk/Source/HRHiringTool/HRHiringTool.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HRHiringTool.DataAccess")]
+[assembly: AssemblyTitle("HRHiringTool.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("HRHiringTool.DataAccess")]
+[assembly: AssemblyProduct("HRHiringTool.Common")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
