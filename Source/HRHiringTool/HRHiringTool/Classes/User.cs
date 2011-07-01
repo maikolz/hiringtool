d3764680752f6da87fff54ed330a1ac983edf413
maikolz@gmail.com <maikolz@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-07-01 22:29:18 +0000

    User initial load

---
+++ Source/HRHiringTool/HRHiringTool/Classes/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRHiringTool.Classes
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+
+    }
+}
