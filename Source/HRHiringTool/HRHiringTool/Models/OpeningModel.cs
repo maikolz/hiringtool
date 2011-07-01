27489c137510b46654624686693eb369f20cc6fe
maikolz@gmail.com <maikolz@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-07-01 02:48:24 +0000

    Opening Model initial load

---
+++ Source/HRHiringTool/HRHiringTool/Models/OpeningModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRHiringTool.Models
+{
+    public class OpeningModel
+    {
+        public OpeningModel() 
+        {
+ 
+        }
+        public IList<string> Departments { get; set; }
+    }
+}
---
+++ Source/HRHiringTool/HRHiringTool/Views/Opening/OpeningDetails.ascx
@@ -0,0 +1,6 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<HRHiringTool.Models.OpeningModel>"%>
+<div>
+<h1>Opening</h1>
+    <asp:DropDownList ID="ddDepartment" runat="server" DataSourceID="<%=Model.Departments %>" >
+    </asp:DropDownList>
+</div>
