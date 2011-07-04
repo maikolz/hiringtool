efc0f4fca38accf39a38a0c263673f45a5d48b6f
rolando.matarrita@gmail.com <rolando.matarrita@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-07-04 06:41:36 +0000

    Added Candidate related files

---
+++ trunk/Source/HRHiringTool/HRHiringTool.DataAccess/Classes/CandidateData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HRHiringTool.DataAccess.Classes
+{
+    class CandidateData
+    {
+        HRHiringTool.DataAccess.Model.HRHiringToolContainer objectContext = new HRHiringTool.DataAccess.Model.HRHiringToolContainer();
+
+        public IList<CandidateData> getCandidateData( long openingId )
+        {
+            IList<CandidateData> returnValue = new List<CandidateData>();
+            // objectContext.Candidate.Select(); ADD LINQ query that obtains candidate data for the opening Id
+            return returnValue;
+        }
+    }
+}
