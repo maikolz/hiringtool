1379e445795a0364253f46bb0b3e35ed415a3f57
rolando.matarrita@gmail.com <rolando.matarrita@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-07-05 05:36:01 +0000

    Se agregó soporte preliminar ajax para el chat history.

---
+++ Source/HRHiringTool/HRHiringTool/Classes/OpeningNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRHiringTool.Classes
+{
+    public class OpeningNote
+    {
+        public string Note { get; set; }
+        public int IdJobOpening { get; set; }
+        public int IdUser { get; set; }
+        public string UserName { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
--- Source/HRHiringTool/HRHiringTool/Global.asax.cs
+++ Source/HRHiringTool/HRHiringTool/Global.asax.cs
@@ -34,7 +34,13 @@ namespace HRHiringTool
         {
             AreaRegistration.RegisterAllAreas();
             if (Application["awareness"] == null)
+            {
                 Application["awareness"] = new Hashtable();
+            }
+            if (Application["newsfeed"] == null)
+            {
+                Application["newsfeed"] = new Hashtable();
+            }
             RegisterRoutes(RouteTable.Routes);
         }
     }
