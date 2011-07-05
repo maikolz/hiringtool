0e8ba0ee1c38c99f564ff3c8af20abd8b76b5877
rolando.matarrita@gmail.com <rolando.matarrita@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-07-05 03:58:07 +0000

    Cambios a Index.aspx y a OpeningController

--- Source/HRHiringTool/HRHiringTool/Controllers/OpeningController.cs
+++ Source/HRHiringTool/HRHiringTool/Controllers/OpeningController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HRHiringTool.DataAccess.Model;
 
 namespace HRHiringTool.Controllers
 {
     public class OpeningController : Controller
     {
+
         //
         // GET: /Opening/
 
@@ -16,5 +18,90 @@ namespace HRHiringTool.Controllers
             return View();
         }
 
+        //
+        // GET: /Opening/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Opening/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Opening/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Opening/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Opening/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Opening/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Opening/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
