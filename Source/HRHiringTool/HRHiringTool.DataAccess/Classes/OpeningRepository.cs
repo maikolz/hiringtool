0dc5c4a64391983a1017dc27e16ed8f5d33b24e8
maikolz@gmail.com <maikolz@gmail.com@f81d791e-4caf-6303-a465-b7747dd542f1>
2011-07-05 06:19:57 +0000

    Opening repository

---
+++ Source/HRHiringTool/HRHiringTool.DataAccess/Classes/OpeningRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HRHiringTool.DataAccess.Model;
+
+namespace HRHiringTool.DataAccess.Classes
+{
+    class OpeningRepository
+    {
+        public IQueryable GetNotes(long jobOpening)
+        {
+            using (var HRHiringToolContext = new HRHiringToolContainer())
+            {
+                var positionNotes = from notes in HRHiringToolContext.OpeningNotes
+                                    where notes.JobOpening.ID_Opening == jobOpening
+                                    orderby notes.DateTime descending
+                                    select notes;
+                return positionNotes;
+            }
+        }
+    }
+}
